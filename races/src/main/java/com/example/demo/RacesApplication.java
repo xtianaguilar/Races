26b82f3955079728a8833afef38832231cd37e48
Christian Aguilar <xtian.aguilar@gmail.com>
2018-10-17 19:21:34 -0500

    Initial project

---
+++ races/src/main/java/com/example/demo/RacesApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacesApplication.class, args);
+	}
+}
