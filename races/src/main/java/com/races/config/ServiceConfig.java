5b84ef0025cc0e2d4fa5171cf2387397c13a1d8e
Christian Aguilar <xtian.aguilar@gmail.com>
2018-12-05 20:32:53 -0600

    service config

--- races/src/main/java/com/races/config/ServiceConfig.java
+++ races/src/main/java/com/races/config/ServiceConfig.java
@@ -1,5 +1,15 @@
 package com.races.config;
 
-public class ServiceConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
+public class ServiceConfig{
+
+  @Value("${example.property}")
+  private String exampleProperty;
+
+  public String getExampleProperty(){
+    return exampleProperty;
+  }
 }
